1f1185fca904eb65ea3b47915630ce26f2c19752
Luis Aguirre <kourosvi@gmail.com>
2017-10-04 15:13:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/admin/newfirebaseproject/MyFirebaseInstanceIdService.java
@@ -0,0 +1,29 @@
+package com.example.admin.newfirebaseproject;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by Admin on 10/4/2017.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+     //   sendRegistrationToServer(refreshedToken);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/admin/newfirebaseproject/MyFirebaseMessagingService.java
@@ -0,0 +1,37 @@
+package com.example.admin.newfirebaseproject;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by Admin on 10/4/2017.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService
+{
+    String TAG = "Firebase";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+}
